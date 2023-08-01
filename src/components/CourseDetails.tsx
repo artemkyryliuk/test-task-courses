cc03bda64c6fb62c0d3533ad19643b036a9070f0
Artem Kyryliuk <no4ta.aptema@gmail.com>
2023-08-01 13:42:36 +0300

    feat: add action buttons and search input

--- src/components/CourseDetails.tsx
+++ src/components/CourseDetails.tsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Paper,
   Table,
   TableBody,
@@ -6,39 +7,55 @@ import {
   TableContainer,
   TableHead,
   TableRow,
+  TextField,
 } from '@mui/material'
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward'
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward'
+
+import ActionButton from './ActionButton'
 import { course1 } from '../data/course-1'
 
 export default function CourseDetails() {
   return (
-    <TableContainer component={Paper}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell align="right"> Topic name </TableCell>
-            <TableCell align="right"> Planned date </TableCell>
-            <TableCell align="right"> Short description </TableCell>
-            <TableCell align="right"> Type </TableCell>
-            <TableCell align="right"> Status </TableCell>
-            <TableCell align="right"> Notes </TableCell>
-          </TableRow>
-        </TableHead>
+    <Box>
+      <TextField label="Search topic by name" />
+      <TableContainer component={Paper}>
+        <Table stickyHeader>
+          <TableHead>
+            <TableRow>
+              <TableCell>
+                Topic name
+                <ActionButton icon={ArrowUpwardIcon} />
+                <ActionButton icon={ArrowDownwardIcon} />
+              </TableCell>
+              <TableCell>
+                Planned date
+                <ActionButton icon={ArrowUpwardIcon} />
+                <ActionButton icon={ArrowDownwardIcon} />
+              </TableCell>
+              <TableCell> Short description </TableCell>
+              <TableCell> Type </TableCell>
+              <TableCell> Status </TableCell>
+              <TableCell> Notes </TableCell>
+            </TableRow>
+          </TableHead>
 
-        <TableBody>
-          {course1.details.map(
-            ({ id, name, date, description, type, completed }) => (
-              <TableRow key={id}>
-                <TableCell> {name} </TableCell>
-                <TableCell> {date} </TableCell>
-                <TableCell> {description} </TableCell>
-                <TableCell> {type}</TableCell>
-                <TableCell>{completed ? 'Passed' : 'Upcoming'}</TableCell>
-                <TableCell> Notes </TableCell>
-              </TableRow>
-            )
-          )}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          <TableBody>
+            {course1.details.map(
+              ({ id, name, date, description, type, completed }) => (
+                <TableRow key={id}>
+                  <TableCell> {name} </TableCell>
+                  <TableCell> {date} </TableCell>
+                  <TableCell> {description} </TableCell>
+                  <TableCell> {type}</TableCell>
+                  <TableCell>{completed ? 'Passed' : 'Upcoming'}</TableCell>
+                  <TableCell> Notes </TableCell>
+                </TableRow>
+              )
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
   )
 }
